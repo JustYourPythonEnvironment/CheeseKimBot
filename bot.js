888e5ea083c26d0f41ee4c0f8a439a363792394e
zuqini <lizuqiliang@gmail.com>
2018-08-18 04:09:06 -0400

    because I can

--- bot.js
+++ bot.js
@@ -6,4 +6,10 @@ client.on('ready', () => {
   console.log('Ready!');
 });
 
+client.on('message', message => {
+  if (message.content.endsWith('Sana!')) {
+    message.channel.send('Shy shy shy!');
+  }
+});
+
 client.login(auth.token);
