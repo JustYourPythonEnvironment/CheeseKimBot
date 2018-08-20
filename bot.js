c6dac862657d54afbac84f2882c8e3bd7c92796f
zuqini <lizuqiliang@gmail.com>
2018-08-20 16:17:56 -0400

    archives recognized media links

--- bot.js
+++ bot.js
@@ -1,15 +1,35 @@
 const Discord = require('discord.js');
 const auth = require('./auth.json');
 const client = new Discord.Client();
+const UrlValidator = require('./utils/UrlValidator.js');
 
 client.on('ready', () => {
   console.log('Ready!');
 });
 
 client.on('message', message => {
+  if (message.author.bot) return;
+
   if (message.content.endsWith('Sana!')) {
     message.channel.send('Shy shy shy!');
   }
+
+  const ytMatch = UrlValidator.matchYTUrl(message.content);
+  const igMatch = UrlValidator.matchIGUrl(message.content);
+  const spMatch = UrlValidator.matchSpotifyUrl(message.content);
+  const vlMatch = UrlValidator.matchVLiveUrl(message.content);
+  if (ytMatch) {
+    client.channels.find(ch => ch.name === 'youtube-links').send(ytMatch[0]);
+  }
+  if (igMatch) {
+    client.channels.find(ch => ch.name === 'instagram-links').send(igMatch[0]);
+  }
+  if (spMatch) {
+    client.channels.find(ch => ch.name === 'spotify-links').send(spMatch[0]);
+  }
+  if (vlMatch) {
+    client.channels.find(ch => ch.name === 'vlive-links').send(vlMatch[0]);
+  }
 });
 
-client.login(auth.token);
+client.login(auth.discordToken);
---
+++ utils/UrlValidator.js
@@ -0,0 +1,25 @@
+const youTubeRegEx = /(http(s)?:\/\/)?((w){3}.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?/;
+const instagramRegEx = /(http(s)?:\/\/)?((w){3}.)?instagram\.com(\/p\/\w+\/?)/;
+const spotifyRegEx = /spotify:track:\w+|(http(s)?:\/\/)?((w){3}.)?[a-z]+\.spotify\.com\/(track\/\w+|user\/[^\s]+)\/?/;
+const vliveRegEx = /(http(s)?:\/\/)?((w){3}.)?vlive\.tv\/video\/\d+/;
+
+function matchStr(str, regex) {
+  if (str) {
+    return str.match(regex);
+  }
+}
+
+module.exports = {
+  matchYTUrl: function(url) {
+    return matchStr(url, youTubeRegEx);
+  },
+  matchIGUrl: function(url) {
+    return matchStr(url, instagramRegEx);
+  },
+  matchSpotifyUrl: function(url) {
+    return matchStr(url, spotifyRegEx);
+  },
+  matchVLiveUrl: function(url) {
+    return matchStr(url, vliveRegEx);
+  }
+};
