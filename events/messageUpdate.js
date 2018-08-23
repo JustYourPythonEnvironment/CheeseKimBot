7ff04468c9e129e7dbf9610a815ece1fc5646045
zuqini <lizuqiliang@gmail.com>
2018-08-23 17:27:03 -0400

    don't archive media when saving with wdl

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,7 +1,7 @@
 const Actions = require('../utils/Actions.js');
 
 module.exports = async (client, oldMessage, newMessage) => {
-  if (newMessage.author.bot) return;
+  if (newMessage.author.bot || client.config.ignorePrefixes.find(prefix => message.content.indexOf(prefix) === 0)) return;
   // only archive new message if message embeds change to avoid cluttering
   let isSame = false;
   if (oldMessage.embeds.length === newMessage.embeds.length) {
