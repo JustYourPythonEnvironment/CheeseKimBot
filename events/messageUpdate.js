3b58fe6cd60a4ccd064ab782ff26434325138df5
zuqini <lizuqiliang@gmail.com>
2018-08-23 15:54:37 -0400

    avoid archiving redundant media when message updates

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -2,6 +2,16 @@ const Actions = require('../utils/Actions.js');
 
 module.exports = async (client, oldMessage, newMessage) => {
   if (newMessage.author.bot) return;
-  Actions.archiveMedia(newMessage);
+  // only archive new message if message embeds change to avoid cluttering
+  let isSame = false;
+  if (oldMessage.embeds.length === newMessage.embeds.length) {
+    isSame = true;
+    oldMessage.embeds.forEach((embed, i) => {
+      if (embed.url !== newMessage.embeds[i].url) {
+        isSame = false;
+      }
+    });
+  }
+  if (!isSame) Actions.archiveMedia(newMessage);
   return;
 };
