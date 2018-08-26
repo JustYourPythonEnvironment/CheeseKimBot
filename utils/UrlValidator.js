33eb83a6f61faaf4b03b3aa09ffe54e54b2b8540
zuqini <lizuqiliang@gmail.com>
2018-08-26 19:51:40 -0400

    generalize spotify regex to accept more spotify links

--- utils/UrlValidator.js
+++ utils/UrlValidator.js
@@ -1,6 +1,6 @@
 const youTubeRegEx = /(http(s)?:\/\/)?((w){3}.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?/;
 const instagramRegEx = /(http(s)?:\/\/)?((w){3}.)?instagram\.com(\/p\/\w+\/?)/;
-const spotifyRegEx = /spotify:track:\w+|(http(s)?:\/\/)?((w){3}.)?[a-z]+\.spotify\.com\/(track\/\w+|user\/[^\s]+)\/?/;
+const spotifyRegEx = /spotify:track:\w+|(http(s)?:\/\/)?((w){3}.)?[a-z]+\.spotify\.com\/[^\s]+/;
 const vliveRegEx = /(http(s)?:\/\/)?((w){3}.)?vlive\.tv\/video\/\d+/;
 
 function matchStr(str, regex) {
