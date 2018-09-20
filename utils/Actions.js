681aefab118227a89c8a09bf34af514d9e333f8a
Jason Manson-Hing <jason.mansonhing@gmail.com>
2018-09-20 13:27:44 -0400

    Added reddit and twitter link archiving

--- utils/Actions.js
+++ utils/Actions.js
@@ -2,47 +2,56 @@ const Attachment = require('discord.js').Attachment;
 const UrlValidator = require('./UrlValidator.js');
 
 archiveMedia = (message) => {
-  console.log(message);
-  const guild = message.guild;
-  if (!guild) return;
+    console.log(message);
+    const guild = message.guild;
+    if (!guild) return;
 
-  const embeds = message.embeds;
-  const attachments = message.attachments;
-  if (embeds) {
-    embeds.forEach((embed) => {
-      const ytMatch = UrlValidator.matchYTUrl(embed.url);
-      const igMatch = UrlValidator.matchIGUrl(embed.url);
-      const spMatch = UrlValidator.matchSpotifyUrl(embed.url);
-      const vlMatch = UrlValidator.matchVLiveUrl(embed.url);
-      if (ytMatch) {
-        const ytChannel = guild.channels.find(ch => ch.name === 'youtube-links');
-        if (ytChannel) ytChannel.send(ytMatch[0]);
-      } else if (igMatch) {
-        const igChannel = guild.channels.find(ch => ch.name === 'instagram-links');
-        if (igChannel) igChannel.send(igMatch[0]);
-      } else if (spMatch) {
-        const spChannel = guild.channels.find(ch => ch.name === 'spotify-links');
-        if (spChannel) spChannel.send(spMatch[0]);
-      } else if (vlMatch) {
-        const vliveChannel = guild.channels.find(ch => ch.name === 'vlive-links');
-        if (vliveChannel) vliveChannel.send(vlMatch[0]);
-      } else if (embed.type === 'link') {
-        const linkChannel = guild.channels.find(ch => ch.name === 'general-links');
-        if (linkChannel) linkChannel.send(embed.url);
-      } else if (embed.type !== 'rich') {
-        const mediaChannel = guild.channels.find(ch => ch.name === 'media');
-        if (mediaChannel) mediaChannel.send(embed.url);
-      }
-    });
-  }
-  if (attachments) {
-    attachments.forEach((attachment) => {
-      const mediaChannel = guild.channels.find(ch => ch.name === 'attachments');
-      if (mediaChannel) mediaChannel.send(new Attachment(attachment.url));
-    });
-  }
+    const embeds = message.embeds;
+    const attachments = message.attachments;
+    if (embeds) {
+        embeds.forEach((embed) => {
+            const ytMatch = UrlValidator.matchYTUrl(embed.url);
+            const igMatch = UrlValidator.matchIGUrl(embed.url);
+            const spMatch = UrlValidator.matchSpotifyUrl(embed.url);
+            const vlMatch = UrlValidator.matchVLiveUrl(embed.url);
+            const twitterMatch = UrlValidator.matchTwitterUrl(embed.url);
+            const redditMatch = UrlValidator.matchRedditUrl(embed.url);
+
+            if (ytMatch) {
+                const ytChannel = guild.channels.find(ch => ch.name === 'youtube-links');
+                if (ytChannel) ytChannel.send(ytMatch[0]);
+            } else if (igMatch) {
+                const igChannel = guild.channels.find(ch => ch.name === 'instagram-links');
+                if (igChannel) igChannel.send(igMatch[0]);
+            } else if (spMatch) {
+                const spChannel = guild.channels.find(ch => ch.name === 'spotify-links');
+                if (spChannel) spChannel.send(spMatch[0]);
+            } else if (vlMatch) {
+                const vliveChannel = guild.channels.find(ch => ch.name === 'vlive-links');
+                if (vliveChannel) vliveChannel.send(vlMatch[0]);
+            } else if (twitterMatch) {
+                const twitterChannel = guild.channels.find(ch => ch.name === 'twitter-links');
+                if (twitterChannel) twitterChannel.send(twitterMatch[0]);
+            } else if (redditMatch) {
+                const redditChannel = guild.channels.find(ch => ch.name === 'reddit-links');
+                if (redditChannel) redditChannel.send(redditMatch[0]);
+            } else if (embed.type === 'link') {
+                const linkChannel = guild.channels.find(ch => ch.name === 'general-links');
+                if (linkChannel) linkChannel.send(embed.url);
+            } else if (embed.type !== 'rich') {
+                const mediaChannel = guild.channels.find(ch => ch.name === 'media');
+                if (mediaChannel) mediaChannel.send(embed.url);
+            }
+        });
+    }
+    if (attachments) {
+        attachments.forEach((attachment) => {
+            const mediaChannel = guild.channels.find(ch => ch.name === 'attachments');
+            if (mediaChannel) mediaChannel.send(new Attachment(attachment.url));
+        });
+    }
 };
 
 module.exports = {
-  archiveMedia,
+    archiveMedia,
 };
--- utils/UrlValidator.js
+++ utils/UrlValidator.js
@@ -2,6 +2,8 @@ const youTubeRegEx = /(http(s)?:\/\/)?((w){3}.)?youtu(?:be\.com\/watch\?.*v=|\.b
 const instagramRegEx = /(http(s)?:\/\/)?((w){3}.)?instagram\.com(\/p\/\w+\/?)/;
 const spotifyRegEx = /spotify:track:\w+|(http(s)?:\/\/)?((w){3}.)?[a-z]+\.spotify\.com\/[^\s]+/;
 const vliveRegEx = /(http(s)?:\/\/)?((w){3}.)?vlive\.tv\/video\/\d+/;
+const twitterRegEx = /(http(s)?:\/\/)?((w){3}.)?twitter\.com\/.*/;
+const redditRegEx = /(http(s)?:\/\/)?((w){3}.)?reddit\.com\/(r|user)\/.*/;
 
 function matchStr(str, regex) {
   if (str) {
@@ -21,5 +23,11 @@ module.exports = {
   },
   matchVLiveUrl: function(url) {
     return matchStr(url, vliveRegEx);
-  }
+  },
+  matchTwitterUrl: function(url) {
+    return matchStr(url, twitterRegEx);
+  },
+  matchRedditUrl: function(url) {
+    return matchStr(url, redditRegEx);
+  },
 };
