a665af435633f3890c2095f39736d83d889733df
William <mawilliamyan@gmail.com>
2018-10-15 23:41:48 -0700

    Updated based on comments.

--- utils/Actions.js
+++ utils/Actions.js
@@ -1,5 +1,6 @@
 const Attachment = require('discord.js').Attachment;
 const UrlValidator = require('./UrlValidator.js');
+const Utils = require('../utils/Utils.js');
 
 const DISCORD_BASE_URL = 'https://discordapp.com';
 
@@ -18,93 +19,90 @@ const archiveMedia = (message) => {
     const guild = message.guild;
     if (!guild) return;
 
-    maybeSendYoutubeLinks(message);
-    maybeSendInstagramLinks(message);
-    maybeSendSpotifyLinks(message);
-    maybeSendVLiveLinks(message);
-    maybeSendTwitterLinks(message);
-    maybeSendRedditLinks(message);
-    maybeSendGeneralLinks(message);
-    maybeSendMediaLinks(message);
+    const embeds = message.embeds;
+    if (embeds) {
+        embeds.forEach(embed => {
+            !maybeSendYoutubeLinks(message, embed) &&
+            !maybeSendInstagramLinks(message, embed) &&
+            !maybeSendSpotifyLinks(message, embed) &&
+            !maybeSendVLiveLinks(message, embed) &&
+            !maybeSendTwitterLinks(message, embed) &&
+            !maybeSendRedditLinks(message, embed) &&
+            !maybeSendGeneralLinks(message, embed) &&
+            !maybeSendMediaLinks(message, embed);
+        });
+    }
     maybeSendAttachments(message);
 };
 
-const maybeSendYoutubeLinks = (message) => {
-    const embeds = message.embeds;
-    if (embeds) {
-        embeds.forEach(embed => {
-            const match = UrlValidator.matchYTUrl(embed.url);
-            if (match) sendToChannel(message, YOUTUBE_LINKS, match[0]);
-        });
+const maybeSendYoutubeLinks = (message, embed) => {
+    const match = UrlValidator.matchYTUrl(embed.url);
+    if (match) {
+        sendToChannel(message, YOUTUBE_LINKS, match[0]);
+        return true;
     }
+    return false;
 }
 
-const maybeSendInstagramLinks = (message) => {
-    const embeds = message.embeds;
-    if (embeds) {
-        embeds.forEach(embed => {
-            const match = UrlValidator.matchIGUrl(embed.url);
-            if (match) sendToChannel(message, INSTAGRAM_LINKS, match[0]);
-        });
+const maybeSendInstagramLinks = (message, embed) => {
+    const match = UrlValidator.matchIGUrl(embed.url);
+    if (match) {
+        sendToChannel(message, INSTAGRAM_LINKS, match[0]);
+        return true;
     }
+    return false;
 }
 
-const maybeSendSpotifyLinks = (message) => {
-    const embeds = message.embeds;
-    if (embeds) {
-        embeds.forEach(embed => {
-            const match = UrlValidator.matchSpotifyUrl(embed.url);
-            if (match) sendToChannel(message, SPOTIFY_LINKS, match[0]);
-        });
+const maybeSendSpotifyLinks = (message, embed) => {
+    const match = UrlValidator.matchSpotifyUrl(embed.url);
+    if (match) {
+        sendToChannel(message, SPOTIFY_LINKS, match[0]);
+        return true;
     }
+    return false;
 }
 
-const maybeSendVLiveLinks = (message) => {
-    const embeds = message.embeds;
-    if (embeds) {
-        embeds.forEach(embed => {
-            const match = UrlValidator.matchVLiveUrl(embed.url);
-            if (match) sendToChannel(message, VLIVE_LINKS, match[0]);
-        });
+const maybeSendVLiveLinks = (message, embed) => {
+    const match = UrlValidator.matchVLiveUrl(embed.url);
+    if (match) {
+        sendToChannel(message, VLIVE_LINKS, match[0]);
+        return true;
     }
+    return false;
 }
 
-const maybeSendTwitterLinks = (message) => {
-    const embeds = message.embeds;
-    if (embeds) {
-        embeds.forEach(embed => {
-            const match = UrlValidator.matchTwitterUrl(embed.url);
-            if (match) sendToChannel(message, TWITTER_LINKS, match[0]);
-        });
+const maybeSendTwitterLinks = (message, embed) => {
+    const match = UrlValidator.matchTwitterUrl(embed.url);
+    if (match) {
+        sendToChannel(message, TWITTER_LINKS, match[0]);
+        return true;
     }
+    return false;
 }
 
-const maybeSendRedditLinks = (message) => {
-    const embeds = message.embeds;
-    if (embeds) {
-        embeds.forEach(embed => {
-            const match = UrlValidator.matchRedditUrl(embed.url);
-            if (match) sendToChannel(message, REDDIT_LINKS, match[0]);
-        });
+const maybeSendRedditLinks = (message, embed) => {
+    const match = UrlValidator.matchRedditUrl(embed.url);
+    if (match) {
+        sendToChannel(message, REDDIT_LINKS, match[0]);
+        return true;
     }
+    return false;
 }
 
-const maybeSendGeneralLinks = (message) => {
-    const embeds = message.embeds;
-    if (embeds) {
-        embeds.forEach(embed => {
-            if (embed.type === 'link') sendToChannel(message, GENERAL_LINKS, embed.url);
-        });
+const maybeSendGeneralLinks = (message, embed) => {
+    if (embed.type === 'link') {
+        sendToChannel(message, GENERAL_LINKS, embed.url);
+        return true;
     }
+    return false;
 }
 
-const maybeSendMediaLinks = (message) => {
-    const embeds = message.embeds;
-    if (embeds) {
-        embeds.forEach(embed => {
-            if (embed.type !== 'rich') sendToChannel(message, MEDIA_LINKS, embed.url);
-        });
+const maybeSendMediaLinks = (message, embed) => {
+    if (embed.type !== 'rich') {
+        sendToChannel(message, MEDIA_LINKS, embed.url);
+        return true;
     }
+    return false;
 }
 
 const maybeSendAttachments = (message) => {
@@ -121,12 +119,18 @@ const sendToChannel = async (originalMessage, channelName, messageToSend, extraC
     const originalChannel = originalMessage.channel;
     const channel = guild.channels.find(ch => ch.name === channelName);
     if (channel) {
-        await channel.send(`${DISCORD_BASE_URL}/channels/${guild.id}/${originalChannel.id}/${originalMessage.id}`);
-        // Hack to tag authors but not ping them.
-        const signature = await channel.send('...');
-        signature.edit(`${originalMessage.author} shared in ${originalChannel} `);
-        if (extraContent) await channel.send(extraContent);
-        channel.send(messageToSend);
+        try {
+            await Utils.sendBlankLine(channel);
+            await channel.send(`${DISCORD_BASE_URL}/channels/${guild.id}/${originalChannel.id}/${originalMessage.id}`);
+            // Hack to tag authors but not ping them.
+            const signature = await channel.send('...');
+            signature.edit(`${originalMessage.author} shared in ${originalChannel} `);
+            if (extraContent) await channel.send(extraContent);
+            await channel.send(messageToSend);
+            await Utils.sendBlankLine(channel);
+        } catch (e) {
+            console.error('Error while archiving media.', e);
+        }
     }
 }
 
--- utils/Utils.js
+++ utils/Utils.js
@@ -1,18 +1,23 @@
 const errorPhrases = require('../assets/errorPhrases.json');
 
-logAndMsg = (channel, msg) => {
+const logAndMsg = (channel, msg) => {
   console.log(msg);
   channel.send(msg);
 };
-getRandomIndex = (arr) => Math.floor(Math.random() * arr.length);
-errAndMsg = (channel, err) => {
+
+const getRandomIndex = (arr) => Math.floor(Math.random() * arr.length);
+
+const errAndMsg = (channel, err) => {
   console.error(err);
   channel.send(`${errorPhrases[getRandomIndex(errorPhrases)]} ${err}`);
 };
 
+// There is actually a character in here to make Discord believe it's not sending an empty message.
+const sendBlankLine = async channel => await channel.send('­');
 
 module.exports = {
   logAndMsg,
   getRandomIndex,
   errAndMsg,
+  sendBlankLine,
 };
